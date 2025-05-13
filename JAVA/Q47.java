ea38395574037e95eb572ce6c570139dccd0b779
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:35:35 +0530

    Create Q47.java

---
+++ JAVA/Q47.java
@@ -0,0 +1,7 @@
+public class CharToString {
+    public static void main(String[] args) {
+        char[] chars = {'J', 'a', 'v', 'a'};
+        String str = new String(chars);
+        System.out.println("Converted String: " + str);
+    }
+}
