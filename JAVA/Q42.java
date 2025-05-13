6356dab02d27d92048e0b6ca76831dc658c8783e
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:31:47 +0530

    Update Q42.java

--- JAVA/Q42.java
+++ JAVA/Q42.java
@@ -1,8 +1,13 @@
 public class MinMax {
     public static void main(String[] args) {
-        int a = 100, b = 50;
+        int i1 = 27;
+        int i2 = -45;
+        double d1 = 84.6;
+        double d2 = 0.45;
 
-        System.out.println("Min: " + Math.min(a, b));
-        System.out.println("Max: " + Math.max(a, b));
+        System.out.println("Minimum out of '" + i1 + "' and '" + i2 + "' = " + Math.min(i1, i2));
+        System.out.println("Maximum out of '" + i1 + "' and '" + i2 + "' = " + Math.max(i1, i2));
+        System.out.println("Minimum out of '" + d1 + "' and '" + d2 + "' = " + Math.min(d1, d2));
+        System.out.println("Maximum out of '" + d1 + "' and '" + d2 + "' = " + Math.max(d1, d2));
     }
 }
