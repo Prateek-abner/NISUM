19c7557bcfce62280317d46e581d7a7cae1055f8
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:27:27 +0530

    Create Q38.java

---
+++ JAVA/Q38.java
@@ -0,0 +1,7 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        StringBuffer sb = new StringBuffer(str);
+        System.out.println("Reversed: " + sb.reverse());
+    }
+}
