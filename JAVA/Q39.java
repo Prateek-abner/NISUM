6a38ee785a08a412340b726dffc2a31087a9e353
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:27:52 +0530

    Create Q39.java

---
+++ JAVA/Q39.java
@@ -0,0 +1,7 @@
+public class Convert {
+    public static void main(String[] args) {
+        Integer num = 42;
+        Float f = num.floatValue(); // Wrapper method
+        System.out.println("Float value: " + f);
+    }
+}
