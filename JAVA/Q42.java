6193eae2195651a9b8ab85636511061a1ea4ee10
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:29:31 +0530

    Create Q42.java

---
+++ JAVA/Q42.java
@@ -0,0 +1,8 @@
+public class MinMax {
+    public static void main(String[] args) {
+        int a = 100, b = 50;
+
+        System.out.println("Min: " + Math.min(a, b));
+        System.out.println("Max: " + Math.max(a, b));
+    }
+}
