159cdfd7eb5cff16b1f3debee5b09a47bbad1c6a
Prateek-abner <148851238+Prateek-abner@users.noreply.github.com>
2025-05-13 10:33:46 +0530

    Create Q44.java

---
+++ JAVA/Q44.java
@@ -0,0 +1,8 @@
+public class StringToInt {
+    public static void main(String[] args) {
+        String numberStr = "100";
+        int number = Integer.parseInt(numberStr);
+
+        System.out.println("Converted int: " + number);
+    }
+}
