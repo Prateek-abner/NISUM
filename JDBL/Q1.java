1636df5401d8146312c512093abacbc7cdfcf1e2
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-05-16 15:09:55 +0530

    Update Q1.java

--- JDBL/Q1.java
+++ JDBL/Q1.java
@@ -1,22 +1,20 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+package org.nisum;
 
-public class DBUtil {
-    public static Connection getConnection() {
-        Connection conn = null;
-        try {
-            
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            String url = "jdbc:mysql://localhost:3306/sql3"; 
-            String user = "root"; 
-            String password = "root@123"; 
-            
-            conn = DriverManager.getConnection(url, user, password);
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-        return conn;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class example {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/jdbc";
+        String username = "root";
+        String password = "root@123";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement statement = connection.createStatement();
+
+       
+        System.out.println("Data inserted successfully!");
     }
 }
