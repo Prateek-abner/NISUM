13ab7738eebc69797153c7f12e64aa9fbbfd85c8
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-05-16 11:39:24 +0530

    Create countinfocls.java

---
+++ JDBL/Q5/countinfocls.java
@@ -0,0 +1,12 @@
+public class CourseInfo {
+    private String courseName;
+    private String instructor;
+    private String grade;
+
+    public CourseInfo(String courseName, String instructor, String grade) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.grade = grade;
+    }
+
+}
