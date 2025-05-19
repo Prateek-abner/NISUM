4d0985ec69e2d441ef16cdc47f909cfb64380f04
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-05-19 08:25:33 +0530

    Update Q4.java

--- DAY5/Q4.java
+++ DAY5/Q4.java
@@ -4,19 +4,18 @@ import java.sql.*;
 
 public class InsertPrepared {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/testdb";
+        String url = "jdbc:mysql://localhost:3306/jdbc";
         String username = "root";
         String password = "root@123";
 
         Connection connection = DriverManager.getConnection(url, username, password);
-        PreparedStatement ps = connection.prepareStatement("INSERT INTO students VALUES (?, ?, ?)");
+        PreparedStatement ps = connection.prepareStatement("INSERT INTO jdbc.newtable1 VALUES (?, ?, ?)");
 
-        ps.setInt(1, 2);
-        ps.setString(2, "Amit");
-        ps.setInt(3, 21);
+        ps.setString(1, "s003");
+        ps.setString(2, "c103");
+        ps.setInt(3, 1);
 
         ps.executeUpdate();
-
         System.out.println("Data inserted using PreparedStatement!");
         connection.close();
     }
