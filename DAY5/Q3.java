fcecb23fe0c091988de592fcb9e161eb380f4089
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-05-19 08:24:10 +0530

    Update Q3.java

--- DAY5/Q3.java
+++ DAY5/Q3.java
@@ -4,19 +4,18 @@ import java.sql.*;
 
 public class ReadData {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/testdb";
+        String url = "jdbc:mysql://localhost:3306/jdbc";
         String username = "root";
         String password = "root@123";
 
         Connection connection = DriverManager.getConnection(url, username, password);
         Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM jdbc.newtable1");
 
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
-
-        while (resultSet.next()) {
-            System.out.println(resultSet.getInt("id") + " " +
-                               resultSet.getString("name") + " " +
-                               resultSet.getInt("age"));
+        while (rs.next()) {
+            System.out.println(rs.getString("student_id") + " " +
+                               rs.getString("course_code") + " " +
+                               rs.getInt("semester"));
         }
 
         connection.close();
