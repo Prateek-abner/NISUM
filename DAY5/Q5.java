ff3137894cb6ed86fa4c6cff3751aadfa8ce2c3b
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-05-19 08:25:03 +0530

    Update Q5.java

--- DAY5/Q5.java
+++ DAY5/Q5.java
@@ -4,18 +4,18 @@ import java.sql.*;
 
 public class UpdateData {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/testdb";
+        String url = "jdbc:mysql://localhost:3306/jdbc";
         String username = "root";
         String password = "root@123";
 
         Connection connection = DriverManager.getConnection(url, username, password);
-        PreparedStatement ps = connection.prepareStatement("UPDATE students SET name = ? WHERE id = ?");
+        PreparedStatement ps = connection.prepareStatement(
+            "UPDATE jdbc.newtable1 SET course_code = ? WHERE student_id = ?");
 
-        ps.setString(1, "Rahul");
-        ps.setInt(2, 1);
+        ps.setString(1, "c999");
+        ps.setString(2, "s001");
 
         ps.executeUpdate();
-
         System.out.println("Record updated!");
         connection.close();
     }
