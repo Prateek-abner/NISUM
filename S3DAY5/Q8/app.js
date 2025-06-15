7ade3e20677a81888d45d7e9ea2e677dfa457e64
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-15 10:51:15 +0530

    Create app.js

---
+++ S3DAY5/Q8/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{ padding: '2rem', textAlign: 'center' }}>
+        <h1>Welcome to My Website</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
