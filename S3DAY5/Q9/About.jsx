8e067a3b526a053193a20ad4841c518385b7074f
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-15 11:18:02 +0530

    Create About.jsx

---
+++ S3DAY5/Q9/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>We are building reusable and scalable UI components in React.</p>
+    </Layout>
+  );
+};
+
+export default About;
