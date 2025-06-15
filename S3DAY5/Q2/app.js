49773aae1fa985cae13a42f72f419f27d47d2c83
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-15 10:20:11 +0530

    Create app.js

---
+++ S3DAY5/Q2/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ProductCard from './ProductCard';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Featured Product</h1>
+
+      
+      <ProductCard 
+        title="Samsung Galaxy S24" 
+        price={999} 
+        description="High-performance phone with AI features." 
+      />
+
+      
+      <ProductCard />
+    </div>
+  );
+}
+
+export default App;
