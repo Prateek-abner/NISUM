b2605fbfd73107e95c29a295c8d1a6f82690504a
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-15 11:16:55 +0530

    Create Home.jsx

---
+++ S3DAY5/Q9/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple homepage using the Layout component.</p>
+    </Layout>
+  );
+};
+
+export default Home;
