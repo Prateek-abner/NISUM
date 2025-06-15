dd18090d94b8565015e01cddb3feacb81afc50dc
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-15 10:49:16 +0530

    Create NavItem.jsx

---
+++ S3DAY5/Q8/NavItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css';
+
+const NavItem = ({ label, isActive, onClick }) => {
+  return (
+    <div
+      className={`nav-item ${isActive ? 'active' : ''}`}
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
+
+export default NavItem;
