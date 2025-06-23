47b59944a215922b3388806b34d57adaf3edddfd
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-06-24 03:10:54 +0530

    Create GreeterAppApplication.java

---
+++ S4DAY1/Assignment1/GreeterAppApplication.java
@@ -0,0 +1,6 @@
+@SpringBootApplication
+public class GreeterAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreeterAppApplication.class, args);
+    }
+}
