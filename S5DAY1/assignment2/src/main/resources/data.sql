ec46074014eeb7d0e8d07c0045ccdf060a203f99
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-07-08 01:46:08 +0530

    Update data.sql

--- S5DAY1/assignment2/src/main/resources/data.sql
+++ S5DAY1/assignment2/src/main/resources/data.sql
@@ -1,1 +1,4 @@
 
+INSERT INTO users (username, password, roles) VALUES
+('user', '$2a$10$8.UnVu0d0y1UXU5JumyqHu3Z1m2W0iS5xqP4mWqT4Z2Q1g7r2q3W.', 'USER'),
+('admin', '$2a$10$8.UnVu0d0y1UXU5JumyqHu3Z1m2W0iS5xqP4mWqT4Z2Q1g7r2q3W.', 'ADMIN');
