03c8d9be1711cdf5ad47a4a1f37b64912282b4ad
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-07-09 10:21:34 +0530

    Create test_controller_testcontainers.java

---
+++ S5DAY2/assignment3/src/main/test/java/com/example/demo/controller/test_controller_testcontainers.java
@@ -0,0 +1,43 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+@ContextConfiguration(initializers = TestControllerTestContainers.Initializer.class)
+class TestControllerTestContainers {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("sa")
+            .withPassword("password");
+
+    @Autowired
+    private TestController testController;
+
+    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(ConfigurableApplicationContext context) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + postgres.getJdbcUrl(),
+                    "spring.datasource.username=" + postgres.getUsername(),
+                    "spring.datasource.password=" + postgres.getPassword()
+            ).applyTo(context.getEnvironment());
+        }
+    }
+
+    @Test
+    void contextLoads() {
+        assertThat(testController).isNotNull();
+    }
+}
