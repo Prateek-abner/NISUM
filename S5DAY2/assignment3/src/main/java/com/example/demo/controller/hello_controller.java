00458230d9543a29528b079e54daab4f7177d037
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-07-09 10:13:27 +0530

    Update hello_controller.java

--- S5DAY2/assignment3/src/main/java/com/example/demo/controller/hello_controller.java
+++ S5DAY2/assignment3/src/main/java/com/example/demo/controller/hello_controller.java
@@ -1,13 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class TestController {
 
-    @GetMapping("/test")
-    public String testEndpoint() {
-        return "Test Endpoint";
+@RestController
+public class HelloController {
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+   
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(required = false, defaultValue = "World") String name) {
+        return helloService.getGreeting(name);
     }
 }
