994bac1b6ad668c739386e311552ad19d9cb8d99
Prateek Parija <148851238+Prateek-abner@users.noreply.github.com>
2025-07-10 09:51:58 +0530

    Create order_controller.java

---
+++ S5DAY3/assignment2/orderservice/src/main/java/com/example/demo/controller/order_controller.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createOrder(@RequestBody String order) {
+        orderService.sendOrder(order);
+        return ResponseEntity.ok("Order created and sent to Kafka");
+    }
+}
